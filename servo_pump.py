1d638f1243a9e0c0f68d6d3721db7616a2564d6f
Adrian Luk <ctluk@uwaterloo.ca>
2024-11-19 13:45:28 -0500

    Add new file

---
+++ servo_pump.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from gpiozero import AngularServo
+
+RELAY_1_GPIO = 26
+SERVO_1_GPIO = 11
+
+GPIO.setmode(GPIO.BCM) # GPIO Numbers instead of board numbers 
+GPIO.setup(RELAY_1_GPIO, GPIO.OUT) # GPIO Assign mode
+GPIO.output(RELAY_1_GPIO, GPIO.LOW)
+
+servo = AngularServo(SERVO_1_GPIO, min_angle =0, max_angle = 270, min_pulse_width =0.5/1000, max_pulse_width=2.5/1000)
+servo.detach()
+
+def set_angle(angle):
+    servo.angle=angle
+    sleep(1)
+
+try:
+    while True:
+        angle = int(input("enter angle: "))
+        set_angle(angle)
+        servo.detach()
+        GPIO.output(RELAY_1_GPIO, GPIO.HIGH)
+        sleep(5)
+        GPIO.output(RELAY_1_GPIO, GPIO.LOW)
+except KeyboardInterrupt:
+    print("stopped")
